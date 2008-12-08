c2da84f5b0b445fe1a4e5c844339b622f71d4a16
Greg Houston <ghouston@yahoo.com>
2008-12-08 10:18:58 -0600

    corrected license for test-tofuhash.rb

--- test-tofuhash.rb
+++ test-tofuhash.rb
@@ -10,32 +10,13 @@ tests to validate similarities between Hash and TofuHash.
 
 Version: see tofuhash.rb for details.
 
-(The MIT License + Free Software Foundation Advertising Prohibition)
+License:
 
-Copyright (c) 2007 Gregory N. Houston
+tofuhash.rb is released under the MIT License + Free Software Foundation
+Advertising Prohibition.  see that file for details.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-Except as contained in this notice, the name(s) of the above copyright holders
-shall not be used in advertising or otherwise to promote the sale, use or other
-dealings in this Software without prior written authorization.
-
+test-tofuhash.rb (this file) is released under the Ruby License since it
+contains source code based on Ruby's released source.
 =end
 
 module TofuHashTesting
